255484564d640de85b330d2bca827eadf4b8f249
blackpintz <rosewanjohi24@gmail.com>
2021-06-28 19:05:21 +0300

    add funds distribution pie chart.

--- components/D3Parse.tsx
+++ components/D3Parse.tsx
@@ -1,18 +1,47 @@
 import React, {useEffect, useState} from 'react';
-import * as d3 from "d3";
+import {pie, arc} from "d3";
+import { makeStyles } from '@material-ui/core/styles';
+import { fundsDistribution } from '../store/whitepaper';
+
+
+const useStyles = makeStyles(() => ({
+    svg: {
+        width: "95%",
+        height: "600px",
+        margin: "1rem auto",
+        border: "2px solid #000"
+    }
+}))
 
 
 const D3Parse = () => {
+    const classes = useStyles();
     const [data, setData] = useState<any>([])
-    useEffect(() => {
-        d3.csv('https://gist.githubusercontent.com/blackpintz/3fe7539cf46a0b91cd0ac60eff356d4f/raw/cssColors.csv')
-            .then(setData)
-        
-        console.log(data)
+    const [width, setWidth] = useState(0)
+    const [height, setHeight] = useState(0)
+    const centerX = width / 2
+    const centerY = height / 2
+    const pieArc = arc()
+          .innerRadius(40)
+          .outerRadius(width * 0.1)
 
+    const fundPie = pie().value(1)
+    useEffect(() => {
+        setData(fundsDistribution);
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight)
     }, [])
+
     return (
-        <></>
+        <>
+            <svg className={classes.svg}>
+                <g transform={`translate(${centerX}, ${centerY})`}>
+                    {fundPie(data).map((item: any)=> (
+                        <path key={item.data['id']} fill={item.data['color']} d={pieArc(item)} />
+                    ))}
+                </g>
+            </svg>
+        </>
     )
 }
 
---
+++ store/whitepaper.tsx
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from 'uuid';
+export const fundsDistribution = [
+    {
+        id: uuidv4(),
+        name: "Acquisitions",
+        value: 75,
+        color: "#F1932D"
+    },
+    {
+        id: uuidv4(),
+        name: "Social Impact",
+        value: 10,
+        color: "#BA8DB4"
+    },
+    {
+        id: uuidv4(),
+        name: "Resources",
+        value: 5,
+        color: "#882E72"
+    },
+    {
+        id: uuidv4(),
+        name: "Operations",
+        value: 5,
+        color: "#1965B0"
+    },
+    {
+        id: uuidv4(),
+        name: "Marketing",
+        value: 2.5,
+        color: "#7BAFDE"
+    },
+    {
+        id: uuidv4(),
+        name: "Legal",
+        value: 2.5,
+        color: "#4EB265"
+    }
+]
